4a44aafb151ee7d6f634052b4112cafd7d87808e
Blaxkk Stone <reachsambath009@gmail.com>
2025-12-30 21:13:44 +0700

    updated

--- src/component/sing-up/index.tsx
+++ src/component/sing-up/index.tsx
@@ -1,3 +1,17 @@
+import { Button, AppBar, Toolbar, Typography } from "@mui/material";
+
 export function SingInComponent() {
-  return <>SingInComponent</>;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Electron + Vite + MUI</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Button variant="contained" sx={{ mt: 2 }}>
+        Click me
+      </Button>
+    </>
+  );
 }
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+// src/renderer/theme.ts
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#d21919ff",
+    },
+  },
+});
